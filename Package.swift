b8da9814581fc5be48165c56c606d4438a6524a1
renwh <frank.ren@aliyun.com>
2021-11-08 14:21:38 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "SQLKitBenchmark", targets: ["SQLKitBenchmark"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/renweihang/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/renweihang/swift-log.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "SQLKit", dependencies: [
